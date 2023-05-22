da900b0ba05e658022046aa303ab334f4cb10889
nicolascou <nicolascousillas1@gmail.com>
2023-05-22 13:11:34 +0200

    add data

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
+import Users from '../data/users.json';
 
 export function getUsers(_: Request, res: Response) {
-  return res.sendStatus(200);
+  return res.json(Users);
 }
 
-export function getUserById(_: Request, res: Response) {
-  return res.sendStatus(200);
+export function getUserById(req: Request, res: Response) {
+  const requestedUser = Users.find(user => user.id === Number(req.params.id));
+  return res.json(requestedUser);
 }
 
 export function createUser(_: Request, res: Response) {
