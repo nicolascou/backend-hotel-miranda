896ccb5b4651c61acf6b0e7b5a16c2d4f81b5602
nicolascou <nicolascousillas1@gmail.com>
2023-05-22 15:50:55 +0200

    add tests for userById and createUser

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -1,9 +1,24 @@
-import supertest from 'supertest';
 import app from '../src/server';
+import supertest from 'supertest';
+
+import Users from '../src/data/users.json';
+
 
 describe('User Routes', () => {
   it('should return users json', async() => {
     const res = await supertest(app).get('/users');
     expect(res.statusCode).toBe(200);
+    expect(res.body).toStrictEqual(Users);
+  });
+
+  it('should return user with id 1', async () => {
+    const res = await supertest(app).get('/users/1');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.id).toBe(1);
+  });
+
+  it('should create user', async () => {
+    const res = await supertest(app).post('/users');
+    expect(res.statusCode).toBe(201);
   });
 });
