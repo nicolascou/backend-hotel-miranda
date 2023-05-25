8490dc2b42546ced3052f3f64cea248f9d82f42f
nicolascou <nicolascousillas1@gmail.com>
2023-05-25 18:19:14 +0200

    quick fix booking validator

--- src/utils/toNewBookings.ts
+++ src/utils/toNewBookings.ts
@@ -1,14 +1,14 @@
 import { INewBooking } from "../models/types";
 import { bookingStatusValidation, stringValidation } from "./validators";
 
-export default function toNewContact(body: any): INewBooking {
+export default function toNewBooking(body: any): INewBooking {
   return {
-    check_in: stringValidation(body.name),
-    check_out: stringValidation(body.email),
-    guest: stringValidation(body.phone),
-    photo: stringValidation(body.subject),
-    room_type: stringValidation(body.comment),
-    special_request: stringValidation(body.archived),
+    check_in: stringValidation(body.check_in),
+    check_out: stringValidation(body.check_out),
+    guest: stringValidation(body.guest),
+    photo: stringValidation(body.photo),
+    room_type: stringValidation(body.room_type),
+    special_request: stringValidation(body.special_request),
     status: bookingStatusValidation(body.status)
   }
 }
