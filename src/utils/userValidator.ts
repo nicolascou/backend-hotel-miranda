00d078c203e22f432660cee12adfde38a4b2d04e
nicolascou <nicolascousillas1@gmail.com>
2023-05-25 17:27:27 +0200

    add user validator

---
+++ src/utils/userValidator.ts
@@ -0,0 +1,46 @@
+import { INewUser } from "../models/types";
+import { BadRequest } from "../models/error";
+
+function stringValidation(s: string) {
+  if ( typeof s !== 'string' || s.length <= 1) {
+    throw new BadRequest('Incorrect user data', 400);
+  }
+  return s;
+}
+
+function stateValidation(state: 'active' | 'inactive') {
+  if (state !== 'active' && state !== 'inactive') {
+    throw new BadRequest('Incorrect user data', 400);
+  }
+  return state;
+}
+
+function emailValidation(email: string) {
+  var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!re.test(email)) {
+    throw new BadRequest('Incorrect or missing email', 400);
+  }
+  return email;
+}
+
+function phoneValidation(phoneNumber: string) {
+  var re = /^\d{10}$/;
+  if (!re.test(phoneNumber)) {
+    throw new BadRequest('Incorrect or missing phone number', 400);
+  }
+  return phoneNumber;
+}
+
+export function toNewUser(body: any): INewUser {
+  return {
+    full_name: stringValidation(body.full_name),
+    description: stringValidation(body.description),
+    email: emailValidation(body.email),
+    password: stringValidation(body.password),
+    photo: stringValidation(body.photo),
+    position: stringValidation(body.photo),
+    state: stateValidation(body.state),
+    username: stringValidation(body.username),
+    phone: phoneValidation(body.phone)
+  }
+}
