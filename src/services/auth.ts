14e23a20afea774a77133e730f2b96f4af86b8db
nicolascou <nicolascousillas1@gmail.com>
2023-06-06 19:47:20 +0200

    add authentication with mongo

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,19 +2,19 @@ import passport from 'passport';
 import { Strategy as localStrategy } from 'passport-local';
 import { Strategy as JWTstrategy } from 'passport-jwt';
 import { ExtractJwt } from 'passport-jwt';
-// import { db } from '../repositories/db';
-// import { RowDataPacket } from 'mysql2';
 import bcrypt from 'bcrypt';
+import { User } from '../repositories/db/models';
+import { IUser } from '../models/types';
 
-// async function validUser(name: string, password: string) {
-// 	const [ results ] = await db.promise().query<RowDataPacket[]>('SELECT username, password FROM users');
-// 	for (const user of results) {
-// 		if (user.username === name && bcrypt.compareSync(password, user.password)) {
-// 			return true;
-// 		}
-// 	}
-// 	return false;
-// }
+async function validUser(name: string, password: string) {
+	const users: IUser[] = await User.find({});
+	for (const user of users) {
+		if (user.username === name && bcrypt.compareSync(password, user.password)) {
+			return true;
+		}
+	}
+	return false;
+}
 
 passport.use(
   'login',
@@ -25,8 +25,8 @@ passport.use(
 	},
 	async (name: string, password: string, done: any) => {
   	try {
-			// const isUser = await validUser(name, password);
-    	if (name === 'nico' && password === '1234') {
+			const isUser = await validUser(name, password);
+    	if (isUser) {
         return done(null, {name}, { message: 'Logged in Successfully' });	 
       }
 			return done(null, false, { message: 'Invalid credentials' });
